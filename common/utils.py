1e803b79692a18a73acec1d4ab5c1a68ba972e6c
Murillo <M.SouzadosSantosPereiraNeto@tudelft.nl>
2025-11-11 16:39:28 -0300

    Initial commit

---
+++ common/utils.py
@@ -0,0 +1,5 @@
+def print_banner():
+    print("###############################################################################################")
+    print("# Surrogate Based Optimization framework for multistage compressor design")
+    print("###############################################################################################")
+    print('\n')
